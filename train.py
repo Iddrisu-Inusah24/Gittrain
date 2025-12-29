35ae6c241a6b98f58cb5abcaad67dc7b5fe228ca
Iddrisu-Inusah24 <iddrisui279@gmail.com>
2025-12-29 13:47:49 +0000

    modified sidebar

--- train.py
+++ train.py
@@ -1,3 +1,6 @@
 import streamlit as st
 st.title("Streamlit is a game changer")
+
+
 st.sidebar.markdown("---")
+st.sidebar.markdown("<h3 style='color:yellow'>Impact Nexus.net</h3>", unsafe_allow_html=True)
